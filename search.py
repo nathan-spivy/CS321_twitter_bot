1717b11f130e9490a4993a1704a7245377d3e146
Nathan Spivy <nathanspivy@gmail.com>
2020-11-18 19:26:31 -0500

    Added function to prevent repeat articles causing twitter post error

--- search.py
+++ search.py
@@ -18,13 +18,35 @@ def search_tweets():
     return tweets
 
 
+def past_tweets():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    # Create API object
+    api = tweepy.API(auth)
+
+    tweets = tweepy.Cursor(api.user_timeline).items(5)
+
+    links = []
+    for tweet in tweets:
+        links.append(tweet._json['entities']['urls'][0]['expanded_url'])
+    return links
+
+
 def get_tweet():
     tweets = search_tweets()
     df = connect()
     for tweet in tweets:
+        # Making sure tweet contains a url
         if len(tweet._json['entities']['urls']) > 0:
             url = tweet._json['entities']['urls'][0]['expanded_url']
-            bias = source_bias(df, url)
-            if len(bias) > 0:
-                return [source_name(df,url),bias,url,tweet]
 
+            # Getting tweet bias
+            bias = source_bias(df, url)
+
+            # Making sure the article isn't posted twice
+            if url in past_tweets():
+                continue
+            # Checks if Bias is valid
+            if len(bias) > 0:
+                return [source_name(df, url), bias, url, tweet]
