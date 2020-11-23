3141db8ed7edc770cc9b203a409da634ea81be7b
Nathan Spivy <nathanspivy@gmail.com>
2020-11-23 18:59:21 -0500

    Added file for testing

---
+++ test.py
@@ -0,0 +1,18 @@
+import unittest
+from database import *
+
+
+class DatabaseTest(unittest.TestCase):
+    def test_source_name(self):
+        df = connect()
+        self.assertEqual(source_name(df, ""), "")
+        self.assertEqual(source_name(df, "random text"), "")
+        self.assertEqual(source_name(df, "https://twitter.com/post/exampletwitter"), "")
+        #self.assertEqual()
+
+    def test_source_bias(self):
+        df = connect()
+        self.assertEqual(source_bias(df, ""), "")
+
+if __name__ == '__main__':
+    unittest.main()
