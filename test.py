c0410fe80fef67916468673f3df00efd143cd52b
Nathan Spivy <nathanspivy@gmail.com>
2020-11-27 22:33:20 -0500

    Added more base test cases.

--- test.py
+++ test.py
@@ -13,7 +13,8 @@ class DatabaseTest(unittest.TestCase):
     def test_source_bias(self):
         df = connect()
         self.assertEqual(source_bias(df, ""), "")
-
+        self.assertEqual(source_bias(df, "random text"), "")
+        self.assertEqual(source_bias(df, "https://twitter.com/post/exampletwitter"), "")
 
 if __name__ == '__main__':
     unittest.main()
